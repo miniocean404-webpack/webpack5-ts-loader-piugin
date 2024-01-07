cc87f8d299ed464f2524465c16deb53639487719
MiniOcean404 <1037306928@qq.com>
2024-01-07 19:59:03 +0800

    chore: 处理打包

--- configs/webpack.config.prod.ts
+++ configs/webpack.config.prod.ts
@@ -4,6 +4,7 @@ import { merge } from "webpack-merge";
 import TerserPlugin from "terser-webpack-plugin";
 import { CleanWebpackPlugin } from "clean-webpack-plugin";
 import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
+import nodeExternals from "webpack-node-externals";
 
 import webpackPaths from "./webpack.paths";
 import baseConfig from "./webpack.base";
@@ -11,13 +12,10 @@ import baseConfig from "./webpack.base";
 const config: Configuration = {
   // 对于 entry 及 loader 的绝对路径 使用 context 作为基础路径
   context: webpackPaths.srcPath,
-
   target: "node",
-
   watch: false,
   mode: "production",
   devtool: "inline-source-map",
-
   entry: {
     loader: { import: "loader.ts", filename: "[name].js" },
     plugin: { import: "plugin.ts", filename: "[name].js" },
@@ -35,14 +33,6 @@ const config: Configuration = {
       umdNamedDefine: true,
     },
   },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        parallel: true,
-      }),
-    ],
-  },
   plugins: [
     // 可视化查看 webpack 打包文件大小分析
     new BundleAnalyzerPlugin({
@@ -59,6 +49,29 @@ const config: Configuration = {
       "process.type": '"loader"',
     }),
   ],
+  // 忽略 node 内置的模块
+  externalsPresets: { node: true },
+  // 处理打包 not cacheable
+  // not cacheable 代表是否使用了缓存
+  cache: {
+    type: "filesystem", //保存位置，开发环境下默认为 memory 类型，生产环境 cache 配置默认是关闭的。
+    // 生产环境下默认的缓存存放目录在 node_modules/.cache/webpack/default-production 中，
+    // 如果想要修改，可通过配置 name，来实现分类存放。如设置 name: 'production-cache' 时生成的缓存
+    name: "click-jump-code",
+    compression: "gzip",
+    buildDependencies: {
+      config: [__filename], // webpack 和 loader是否失效来决定之前构建是否失效
+    },
+    version: "1.0",
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+      }),
+    ],
+  },
 
   // 禁止 webpack 处理 __dirname 和 __filename.
   node: {
